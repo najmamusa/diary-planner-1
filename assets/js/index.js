85163025128f1989295b01cfe1ac94a60be49497
Najima Musa <najmusaa@gmail.com>
2024-01-25 00:05:04 +0000

    changes to the js and css

---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+const localeSettings = {};
+dayjs.locale(localeSettings);
+$(function () {
+    const currentHour = dayjs().format("H");
+    function hourlyColor() {
+        $(".time-block").each(function () {
+          const blockHour = parseInt(this.id);
+          $(this).toggleClass("past", blockHour < currentHour);
+          $(this).toggleClass("present", blockHour === currentHour);
+          $(this).toggleClass("future", blockHour > currentHour);
+        });
+}
+
+function refreshColor() {
+    $(".time-block").each(function () {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else if (blockHour < currentHour) {
+        $(this).removeClass("future present").addClass("past");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  }
+
+  $(".time-block").each(function () {
+    const key = $(this).attr("id");
+    const value = localStorage.getItem(key);
+    $(this).children(".description").val(value);
+  });
+
+  // Realtime clock
+  function updateTime() {
+    const dateElement = $("#date");
+    const timeElement = $("#time");
+    const currentDate = dayjs().format("dddd, D MMMM, YYYY");
+    const currentTime = dayjs().format("hh:mm:ss A");
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
+  // Main functions call
+  hourlyColor();
+  textEntry();
+  refreshColor();
+  setInterval(updateTime, 1000);
+});
